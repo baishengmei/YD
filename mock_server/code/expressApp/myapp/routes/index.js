1530b84dea719e40362001f72333344d3a72bf7b
baishengmei <1023638368@qq.com>
2017-05-05 20:43:18 +0800

    temp

--- mock_server/code/expressApp/myapp/routes/index.js
+++ mock_server/code/expressApp/myapp/routes/index.js
@@ -1,10 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/*', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
-  res.send('succeed!');
+var admin = express();
+var app = express();
+
+admin.on('mount', function (parent) {
+	console.log('admin mounted!');
+	// console.log(parent);
+});
+admin.get('/', function (req, res) {
+	res.send("admin homepage!");
+});
+app.use('/', admin);
+app.all('/aa', function (req, res, err) {
+	res.send("app.all");
 });
 
-module.exports = router;
+
+module.exports = app;
+
+
+/* GET home page. */
+// router.get('/*', function(req, res, next) {
+//   // res.render('index', { title: 'Express' });
+//   res.send('succeed!');
+// });
+
+// module.exports = router;
