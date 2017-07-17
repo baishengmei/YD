c5ed669abf614e02b3f957cd37fe34de753893eb
baishengmei <1023638368@qq.com>
2017-07-17 14:08:29 +0800

    若url已存在，则更新；否则直接插入

--- mock_server/code/myapp_配置页面/src/middlewares/saveRules.js
+++ mock_server/code/myapp_配置页面/src/middlewares/saveRules.js
@@ -5,7 +5,7 @@ var DB_CONN_STR = 'mongodb://localhost:27017/mockserver'; // 数据库为 mockse
 export default async(req, res) => {
 
 	// let url2ruleName = {};
-	let ruleName2rules = {};
+	let url2rules = {};
 	let reqB = {};
   let reqH = {};
   let reqQ = {};
@@ -43,7 +43,7 @@ export default async(req, res) => {
   reqH = isUndefined(body.request.$h);
   reqQ = isUndefined(body.request.$q);
 	//将接收到的body数据进行格式处理，保存成待保存的数据格式,实际保存的url链接为"项目组名/url"
-	ruleName2rules["/" + body.projectName + "/" +body.request.$u] = {
+	url2rules["/" + body.projectName + "/" +body.request.$u] = {
     "rulename": body.ruleName,
 		"reqc": body.request.$c,
 		"reqm": body.request.$m,
@@ -54,30 +54,32 @@ export default async(req, res) => {
 
   //用于保存约束条件
   if(body.request.$G == undefined){
-    ruleName2rules["/" + body.projectName + "/" +body.request.$u]["G1"] = "";
+    url2rules["/" + body.projectName + "/" +body.request.$u]["G1"] = "";
   }else{
     for(let i in body.request.$G){
-      ruleName2rules["/" + body.projectName + "/" +body.request.$u][i.substring(1)] = body.request.$G[i];
+      url2rules["/" + body.projectName + "/" +body.request.$u][i.substring(1)] = body.request.$G[i];
     }
   }
 
   //用于将配置页面的响应部分传给node后端
   response = body.response.notempty();
   for(let i=0; i<response.length; i++){
-    ruleName2rules["/" + body.projectName + "/" +body.request.$u]['constrRes'] = [];
-    ruleName2rules["/" + body.projectName + "/" +body.request.$u]['constrRes'][i] = {};
-    ruleName2rules["/" + body.projectName + "/" +body.request.$u]['constrRes'][i]['condition'] = response[i]['$i'];
-    ruleName2rules["/" + body.projectName + "/" +body.request.$u]['constrRes'][i]['ressc'] = response[i]['$sc'];
-    ruleName2rules["/" + body.projectName + "/" +body.request.$u]['constrRes'][i]['resc'] = response[i]['$c'];
-    ruleName2rules["/" + body.projectName + "/" +body.request.$u]['constrRes'][i]['response'] = response[i]['response'];
+    url2rules["/" + body.projectName + "/" +body.request.$u]['constrRes'] = [];
+    url2rules["/" + body.projectName + "/" +body.request.$u]['constrRes'][i] = {};
+    url2rules["/" + body.projectName + "/" +body.request.$u]['constrRes'][i]['condition'] = response[i]['$i'];
+    url2rules["/" + body.projectName + "/" +body.request.$u]['constrRes'][i]['ressc'] = response[i]['$sc'];
+    url2rules["/" + body.projectName + "/" +body.request.$u]['constrRes'][i]['resc'] = response[i]['$c'];
+    url2rules["/" + body.projectName + "/" +body.request.$u]['constrRes'][i]['response'] = response[i]['response'];
   }
 
   //将rules和names出入到数据库
   let insertRules = function(db, callback) {  
     //连接到表 site
     let collection = db.collection('rules_site');
+    //keypath的值
+    let keypathVal = "/" + body.projectName + "/" +body.request.$u;
     //插入数据
-    collection.insert({'keypath': "/" + body.projectName + "/" +body.request.$u, 'value':ruleName2rules["/" + body.projectName + "/" +body.request.$u]}, function(err, result) { 
+    collection.insert({'keypath': keypathVal, 'value':url2rules[keypathVal]}, function(err, result) { 
         if(err)
         {
             console.log('Error:'+ err);
@@ -88,7 +90,16 @@ export default async(req, res) => {
   }
   MongoClient.connect(DB_CONN_STR, function(err, db) {
       console.log("连接成功！");
-      db.collection('rules_site').find({'keypath': })
+      //keypath的值
+      let keypathVal = "/" + body.projectName + "/" +body.request.$u;
+      //数据库查询是否存在当前保存的url，如果存在则更新，如果不存在则保存，更新的方式是：先删除，后插入，当然这里也可以采用update的方式进行更新
+      db.collection('rules_site').find({'keypath': keypathVal}, function(err, cursor){
+        cursor.each(function(error,doc){
+          if(doc){
+            db.collection('rules_site').remove({'keypath': keypathVal});
+          }
+        }); 
+      })
       insertRules(db, function(result) {
           console.log(result);
           db.close();
@@ -96,5 +107,5 @@ export default async(req, res) => {
   });
   
 	res.status(200).send({status: "succeed!"});
-	console.log(ruleName2rules,"ruleName2rules的值") 
+	console.log(url2rules,"url2rules的值");
 };
